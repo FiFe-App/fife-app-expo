49a63c6b2d02bea39a9b3c04dcfb0d4e13095418
Akos <kristofakos1229@gmail.com>
2024-11-03 13:43:16 +0100

    startup fix

--- app/index.tsx
+++ app/index.tsx
@@ -1,22 +1,15 @@
 import Smiley from "@/components/Smiley";
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
-import { logout } from "@/lib/redux/reducers/userReducer";
 import { RootState } from "@/lib/redux/store";
 import { UserState } from "@/lib/redux/store.type";
 import { Link, Redirect } from "expo-router";
 import { View } from "react-native";
 import { Button, Text } from "react-native-paper";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 
 export default function Index() {
-  const { uid, name }: UserState = useSelector(
-    (state: RootState) => state.user,
-  );
-  const dispatch = useDispatch();
-  const startLogout = () => {
-    dispatch(logout());
-  };
+  const { uid }: UserState = useSelector((state: RootState) => state.user);
 
   if (uid)
     return (
@@ -41,42 +34,18 @@ export default function Index() {
       >
         <Smiley style={{ width: 100, height: 100 }} />
         <Text style={{ zIndex: -1, textAlign: "center" }}>
-          <ThemedText style={{}}>Üdövözzlek a FiFe Appban!</ThemedText>
+          <ThemedText style={{}}>Üdvözöllek a FiFe Appban!</ThemedText>
           {"\n"}
           <ThemedText>Ez egy szerető budapesti közösség.</ThemedText>
-          {"\n"}
-          {uid && (
-            <ThemedText type="defaultSemiBold">
-              Bejelentkezve, mint {name}
-            </ThemedText>
-          )}
         </Text>
       </View>
       <View style={{ margin: 8, gap: 16, marginBottom: 24, zIndex: -1 }}>
-        {uid ? (
-          <>
-            <Button mode="contained-tonal" onPress={startLogout}>
-              Kijelentkezés
-            </Button>
-            <View style={{ gap: 8 }}>
-              <Link href="/user" asChild>
-                <Button mode="contained">Profilom</Button>
-              </Link>
-              <Link href="/biznisz" asChild>
-                <Button mode="contained">Biznisz</Button>
-              </Link>
-            </View>
-          </>
-        ) : (
-          <>
-            <Link href="/csatlakozom" asChild>
-              <Button mode="contained">Mi is ez?</Button>
-            </Link>
-            <Link href="/login" asChild>
-              <Button mode="contained-tonal">Bejelentkezés</Button>
-            </Link>
-          </>
-        )}
+        <Link href="/csatlakozom" asChild>
+          <Button mode="contained">Mi is ez?</Button>
+        </Link>
+        <Link href="/login" asChild>
+          <Button mode="contained-tonal">Bejelentkezés</Button>
+        </Link>
       </View>
     </ThemedView>
   );
