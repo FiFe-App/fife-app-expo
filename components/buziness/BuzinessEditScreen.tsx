70e397a3d2732d89232d013cac0fa2de8c7e6e4d
Akos <kristofakos1229@gmail.com>
2024-11-08 09:29:47 +0100

    buziness fix?

--- components/buziness/BuzinessEditScreen.tsx
+++ components/buziness/BuzinessEditScreen.tsx
@@ -10,7 +10,7 @@ import { UserState } from "@/lib/redux/store.type";
 import { supabase } from "@/lib/supabase/supabase";
 import { router, useFocusEffect, useNavigation } from "expo-router";
 import { useCallback, useEffect, useState } from "react";
-import { View } from "react-native";
+import { ScrollView, View } from "react-native";
 import {
   Button,
   Card,
@@ -130,22 +130,16 @@ export default function BuzinessEditScreen({
             if (editingBuziness?.author !== uid) return;
             if (editingBuziness) {
               setNewBuziness({
-                title: editingBuziness.title.split(" ")[0],
+                title: editingBuziness.title.split(" $ ")[0],
                 description: editingBuziness.description,
               });
-              console.log(
-                "CATEGORIES",
-                editingBuziness.title
-                  .split(" ")
-                  .slice(1)
-                  .reduce((partialSum, a) => partialSum + a, ""),
-              );
 
               setCategories(
                 editingBuziness.title
-                  .split(" ")
+                  .split(" $ ")
                   .slice(1)
-                  .reduce((partialSum, a) => partialSum + " " + a, "") + " ",
+                  .reduce((partialSum, a) => partialSum + " $ " + a, "") +
+                  " $ ",
               );
 
               const cords = locationToCoords(String(editingBuziness.location));
@@ -169,149 +163,151 @@ export default function BuzinessEditScreen({
 
   return (
     <ThemedView style={{ flex: 1 }}>
-      {tutorialVisible && (
-        <Card
-          mode="elevated"
-          style={{ borderTopLeftRadius: 0, borderTopRightRadius: 0 }}
-        >
-          <Card.Title
-            title={"Mihez értesz?"}
-            right={() => (
-              <IconButton
-                icon="close"
-                onPress={() => setTutorialVisible(false)}
-              />
-            )}
-          />
-          <Card.Content>
-            {tutorialVisible && (
-              <Text style={{ textAlign: "justify" }}>
-                Ezen az oldalon fel tudsz venni egy új bizniszt a profilodba.
-                {"\n"}A te bizniszeid azon hobbijaid, képességeid vagy szakmáid
-                listája, amelyeket meg szeretnél osztani másokkal is. {"\n"}Ha
-                te mondjuk úgy gyártod a sütiket, mint egy gép, és ezt felveszed
-                a bizniszeid közé, a Biznisz oldalon megtalálható leszel a süti
-                kulcsszóval.
-              </Text>
-            )}
-          </Card.Content>
-        </Card>
-      )}
-      <View style={{ flex: 1 }}>
-        <TextInput
-          placeholder="Bizniszem neve"
-          value={newBuziness.title}
-          onChangeText={(t) =>
-            setNewBuziness({ ...newBuziness, title: t.trim() })
-          }
-        />
-        <TagInput
-          placeholder="Kategóriái"
-          onChange={setCategories}
-          value={categories}
-        />
-        <TextInput
-          placeholder="Fejtsd ki bővebben"
-          value={newBuziness.description}
-          multiline
-          onChangeText={(t) =>
-            setNewBuziness({ ...newBuziness, description: t.trim() })
-          }
-        />
-        <Card>
-          <Card.Title
-            title="A bizniszed helyzete:"
-            right={() => (
-              <IconButton
-                icon="help-circle-outline"
-                onPress={() => setLocationTutorialVisible(true)}
-              />
-            )}
-          />
-          <Card.Content>
-            {locationTutorialVisible && (
-              <Text>
-                A helyzet fontos, ugyanis kereséskor mindig a közelebbi
-                bizniszek jönnek először.
-              </Text>
-            )}
-            <View style={{ flexDirection: "row", alignItems: "center" }}>
-              <View style={{ flex: 1 }}>
-                {!!locationError && !circle && (
-                  <Text>
-                    <Icon size={16} source="map-marker-question" />
-                    {locationError}
-                  </Text>
-                )}
-                {!!circle ? (
-                  <Text>
-                    <Icon size={16} source="map-marker-account" />
-                    Hely kiválasztva
-                  </Text>
-                ) : (
-                  !!myLocation && (
-                    <Text>
-                      <Icon size={16} source="map-marker" />
-                      Jelenlegi helyzeted használata.
-                    </Text>
-                  )
-                )}
-              </View>
-              <Button onPress={() => setMapModalVisible(true)}>
-                {!!circle ? "Környék kiválasztva" : "Válassz környéket"}
-              </Button>
-            </View>
-          </Card.Content>
-        </Card>
-        <MapView
-          options={{
-            mapTypeControl: false,
-            fullscreenControl: false,
-            streetViewControl: false,
-          }}
-          zoomControlEnabled={false}
-          initialCamera={{
-            altitude: 10,
-            center: selectedLocation ||
-              myLocation?.coords || {
-                latitude: 47.4979,
-                longitude: 19.0402,
-              },
-            heading: 0,
-            pitch: 0,
-            zoom: 12,
-          }}
-          provider="google"
-          googleMapsApiKey={process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY}
-          pitchEnabled={false}
-          rotateEnabled={false}
-          toolbarEnabled={false}
-        >
-          {(!!selectedLocation || !!myLocation) && (
-            <Marker
-              coordinate={
-                selectedLocation ||
-                myLocation?.coords || {
-                  latitude: 47.4979,
-                  longitude: 19.0402,
-                }
-              }
+      <ScrollView style={{ flex: 1 }} contentContainerStyle={{}}>
+        {tutorialVisible && (
+          <Card
+            mode="elevated"
+            style={{ borderTopLeftRadius: 0, borderTopRightRadius: 0 }}
+          >
+            <Card.Title
+              title={"Mihez értesz?"}
+              right={() => (
+                <IconButton
+                  icon="close"
+                  onPress={() => setTutorialVisible(false)}
+                />
+              )}
             />
-          )}
-        </MapView>
-      </View>
-
-      <Portal>
-        <Modal
-          visible={mapModalVisible}
-          onDismiss={() => {
-            setMapModalVisible(false);
-          }}
-          contentContainerStyle={containerStyle}
-        >
-          <MapSelector data={circle} setData={setCircle} searchEnabled />
-        </Modal>
-      </Portal>
+            <Card.Content>
+              {tutorialVisible && (
+                <Text style={{ textAlign: "justify" }}>
+                  Ezen az oldalon fel tudsz venni egy új bizniszt a profilodba.
+                  {"\n"}A te bizniszeid azon hobbijaid, képességeid vagy
+                  szakmáid listája, amelyeket meg szeretnél osztani másokkal is.{" "}
+                  {"\n"}Ha te mondjuk úgy gyártod a sütiket, mint egy gép, és
+                  ezt felveszed a bizniszeid közé, a Biznisz oldalon
+                  megtalálható leszel a süti kulcsszóval.
+                </Text>
+              )}
+            </Card.Content>
+          </Card>
+        )}
+        <View style={{}}>
+          <TextInput
+            placeholder="Bizniszem neve"
+            value={newBuziness.title}
+            onChangeText={(t) => setNewBuziness({ ...newBuziness, title: t })}
+          />
+          <TagInput
+            placeholder="Kategóriái"
+            onChange={setCategories}
+            value={categories}
+          />
+          <TextInput
+            placeholder="Fejtsd ki bővebben"
+            value={newBuziness.description}
+            multiline
+            onChangeText={(t) =>
+              setNewBuziness({ ...newBuziness, description: t })
+            }
+          />
+          <Card>
+            <Card.Title
+              title="A bizniszed helyzete:"
+              right={() => (
+                <IconButton
+                  icon="help-circle-outline"
+                  onPress={() => setLocationTutorialVisible(true)}
+                />
+              )}
+            />
+            <Card.Content>
+              {locationTutorialVisible && (
+                <Text>
+                  A helyzet fontos, ugyanis kereséskor mindig a közelebbi
+                  bizniszek jönnek először.
+                </Text>
+              )}
+              <View style={{ flexDirection: "row", alignItems: "center" }}>
+                <View style={{ flex: 1 }}>
+                  {!!locationError && !circle && (
+                    <Text>
+                      <Icon size={16} source="map-marker-question" />
+                      {locationError}
+                    </Text>
+                  )}
+                  {!!circle ? (
+                    <Text>
+                      <Icon size={16} source="map-marker-account" />
+                      Hely kiválasztva
+                    </Text>
+                  ) : (
+                    !!myLocation && (
+                      <Text>
+                        <Icon size={16} source="map-marker" />
+                        Jelenlegi helyzeted használata.
+                      </Text>
+                    )
+                  )}
+                </View>
+                <Button onPress={() => setMapModalVisible(true)}>
+                  {!!circle ? "Környék kiválasztva" : "Válassz környéket"}
+                </Button>
+              </View>
+            </Card.Content>
+          </Card>
+          <View style={{ minHeight: 300 }}>
+            <MapView
+              options={{
+                mapTypeControl: false,
+                fullscreenControl: false,
+                streetViewControl: false,
+              }}
+              zoomControlEnabled={false}
+              initialCamera={{
+                altitude: 10,
+                center: selectedLocation ||
+                  myLocation?.coords || {
+                    latitude: 47.4979,
+                    longitude: 19.0402,
+                  },
+                heading: 0,
+                pitch: 0,
+                zoom: 12,
+              }}
+              style={{}}
+              provider="google"
+              googleMapsApiKey={process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY}
+              pitchEnabled={false}
+              rotateEnabled={false}
+              toolbarEnabled={false}
+            >
+              {(!!selectedLocation || !!myLocation) && (
+                <Marker
+                  coordinate={
+                    selectedLocation ||
+                    myLocation?.coords || {
+                      latitude: 47.4979,
+                      longitude: 19.0402,
+                    }
+                  }
+                />
+              )}
+            </MapView>
+          </View>
+        </View>
+        <Portal>
+          <Modal
+            visible={mapModalVisible}
+            onDismiss={() => {
+              setMapModalVisible(false);
+            }}
+            contentContainerStyle={containerStyle}
+          >
+            <MapSelector data={circle} setData={setCircle} searchEnabled />
+          </Modal>
+        </Portal>
+      </ScrollView>
     </ThemedView>
   );
 }
