1f8b45b36be8be258992e71348b9ba3cd8404921
Akos <kristofakos1229@gmail.com>
2025-01-29 16:57:29 +0100

    refactor: remove ContactEditScreen components and update userReducer to merge user data

--- app/user/edit.tsx
+++ app/user/edit.tsx
@@ -1,17 +1,19 @@
+import ContactEditScreen from "@/components/buziness/ContactEditScreen";
 import ProfileImage from "@/components/ProfileImage";
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
 import { Tables } from "@/database.types";
 import { supabase } from "@/lib/supabase/supabase";
-import { setOptions } from "@/redux/reducers/infoReducer";
+import { addDialog, setOptions } from "@/redux/reducers/infoReducer";
 import { setName, setUserData } from "@/redux/reducers/userReducer";
 import { RootState } from "@/redux/store";
 import { UserState } from "@/redux/store.type";
+import { PostgrestSingleResponse } from "@supabase/supabase-js";
 import * as ExpoImagePicker from "expo-image-picker";
 import { router, useFocusEffect } from "expo-router";
-import { useCallback, useState } from "react";
-import { View } from "react-native";
-import { IconButton, TextInput } from "react-native-paper";
+import { useCallback, useRef, useState } from "react";
+import { ScrollView, View } from "react-native";
+import { IconButton, Text, TextInput } from "react-native-paper";
 import { useDispatch, useSelector } from "react-redux";
 
 type UserInfo = Partial<Tables<"profiles">>;
@@ -24,6 +26,9 @@ export default function Index() {
   const [imageLoading, setImageLoading] = useState(false);
   const [profile, setProfile] = useState<UserInfo>({});
   const dispatch = useDispatch();
+  const contactEditRef = useRef<{
+    saveContacts: () => Promise<PostgrestSingleResponse<null>>;
+  }>();
 
   const load = () => {
     console.log("loaded user", myUid);
@@ -48,15 +53,17 @@ export default function Index() {
   };
   useFocusEffect(
     useCallback(() => {
-      const save = () => {
+      const save = async () => {
         setLoading(true);
         if (!myUid) return;
 
-        console.log({
-          ...profile,
-          id: myUid,
-        });
+        const response = await contactEditRef.current?.saveContacts();
+        console.log(response);
 
+        if (response?.error) {
+          console.log(response.error);
+          return;
+        }
         supabase
           .from("profiles")
           .upsert(
@@ -156,40 +163,51 @@ export default function Index() {
 
     return upload;
   };
-
   if (myUid)
     return (
       <ThemedView style={{ flex: 1 }}>
-        <View style={{ alignItems: "center", marginBottom: 16 }}>
-          <View style={{ width: 100 }}>
-            <ProfileImage
-              key={profile?.avatar_url}
-              uid={myUid}
-              avatar_url={profile?.avatar_url}
-              propLoading={imageLoading}
-              style={{
-                width: 100,
-                height: 100,
-              }}
-            />
-            <IconButton
-              icon="upload"
-              onPress={pickImage}
-              mode="contained-tonal"
-              style={{ position: "absolute", right: 0, bottom: 0 }}
-            />
+        <ScrollView style={{ flex: 1, padding: 8 }}>
+          <View style={{ alignItems: "center", marginBottom: 16 }}>
+            <View style={{ width: 100 }}>
+              <ProfileImage
+                key={profile?.avatar_url}
+                uid={myUid}
+                avatar_url={profile?.avatar_url}
+                propLoading={imageLoading}
+                style={{
+                  width: 100,
+                  height: 100,
+                }}
+              />
+              <IconButton
+                icon="upload"
+                onPress={pickImage}
+                mode="contained-tonal"
+                style={{ position: "absolute", right: 0, bottom: 0 }}
+              />
+            </View>
           </View>
-        </View>
-        <TextInput
-          label="Teljes név"
-          value={profile?.full_name || ""}
-          disabled={loading}
-          onChangeText={(t) => setProfile({ ...profile, full_name: t })}
-        />
-        <View style={{ padding: 16 }}>
-          <ThemedText type="label">Email</ThemedText>
-          <ThemedText>{userData?.email}</ThemedText>
-        </View>
+          <TextInput
+            label="Teljes név"
+            value={profile?.full_name || ""}
+            disabled={loading}
+            onChangeText={(t) => setProfile({ ...profile, full_name: t })}
+          />
+          <View style={{ padding: 16 }}>
+            <ThemedText type="label">Email, amivel regisztráltál:</ThemedText>
+            <ThemedText>{userData?.email}</ThemedText>
+          </View>
+          <View style={{ gap: 8 }}>
+            <ThemedText type="subtitle">Elérhetőségeid</ThemedText>
+            <ThemedText
+              style={{ textAlign: "center", margin: 16 }}
+              type="defaultSemiBold"
+            >
+              Figyelem! Az alábbi adatok láthatóak minden felhasználónak.
+            </ThemedText>
+            <ContactEditScreen ref={contactEditRef} />
+          </View>
+        </ScrollView>
       </ThemedView>
     );
 }
--- components/buziness/ContactEditScreen.tsx
+++ components/buziness/ContactEditScreen.tsx
@@ -1,7 +1,5 @@
 import { ThemedText } from "@/components/ThemedText";
-import { ThemedView } from "@/components/ThemedView";
 import { Enums, Tables } from "@/database.types";
-import typeToIcon from "@/lib/functions/typeToIcon";
 import typeToPlaceholder from "@/lib/functions/typeToPlaceholder";
 import typeToPrefix from "@/lib/functions/typeToPrefix";
 import typeToValueLabel from "@/lib/functions/typeToValueLabel";
@@ -10,24 +8,16 @@ import { supabase } from "@/lib/supabase/supabase";
 import { addDialog, setOptions } from "@/redux/reducers/infoReducer";
 import { RootState } from "@/redux/store";
 import { router, useFocusEffect } from "expo-router";
-import React, { useCallback, useState } from "react";
+import React, {
+  forwardRef,
+  useCallback,
+  useImperativeHandle,
+  useState,
+} from "react";
 import { View } from "react-native";
-import {
-  Button,
-  Divider,
-  Headline,
-  Icon,
-  List,
-  MD3DarkTheme,
-  Switch,
-  Text,
-  TextInput,
-  TouchableRipple,
-} from "react-native-paper";
-import { Dropdown, DropdownInputProps } from "react-native-paper-dropdown";
+import { Button, Text, TextInput } from "react-native-paper";
 import { trackPromise } from "react-promise-tracker";
 import { useDispatch, useSelector } from "react-redux";
-import TutorialCard from "../TutorialCard";
 
 const types: {
   label: string;
@@ -42,247 +32,143 @@ const types: {
   { label: "Más", value: "OTHER" },
 ];
 
-type IContact = Partial<Tables<"contacts">>;
+type Optional<T, K extends keyof T> = Pick<Partial<T>, K> & Omit<T, K>;
 
-const ContactEditScreen = ({ id }: { id?: string }) => {
-  const [loading, setLoading] = useState(false);
-  const { uid } = useSelector((state: RootState) => state.user);
-  const dispatch = useDispatch();
-  const [contact, setContact] = useState<IContact>({
-    data: "",
-    title: "",
-    public: true,
-    author: uid,
-  });
-  const [error, setError] = useState<string | null>(null);
+type IContact = Optional<Tables<"contacts">, "id"> | undefined;
 
-  const loadContacts = () => {
-    console.log(id);
+const ContactEditScreen = forwardRef<{ saveContacts: () => void }>(
+  (_props, ref) => {
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState<{
+      type: Enums<"contact_type">;
+      text: string;
+    } | null>(null);
+    const { uid, userData } = useSelector((state: RootState) => state.user);
+    const dispatch = useDispatch();
+    const [contacts, setContacts] = useState<IContact[]>([]);
+    const loadContacts = () => {
+      if (uid)
+        supabase
+          .from("contacts")
+          .select("*")
+          .eq("author", uid)
+          .then((res) => {
+            if (res.data?.length) setContacts(res.data);
+            setLoading(false);
+          });
+    };
+    console.log(contacts);
 
-    if (uid && id && id !== "index")
-      supabase
-        .from("contacts")
-        .select("*")
-        .eq("id", id)
-        .then((res) => {
-          if (res.data?.length) setContact(res.data[0]);
-          else {
-            setError("Nincs ilyen elérhetőség");
-          }
-        });
-  };
-  useFocusEffect(
-    useCallback(() => {
-      if (id)
-        dispatch(
-          setOptions([
-            {
-              title: "Törlés",
-              icon: "delete",
-              onPress: () => {
-                dispatch(
-                  addDialog({
-                    title: "Elérhetőség törlése?",
-                    text: "Nem fogod tudni visszavonni.",
-                    onSubmit: () => {
-                      setLoading(true);
-                      trackPromise(
-                        wrapper<null, any>(
-                          supabase
-                            .from("contacts")
-                            .delete()
-                            .eq("id", id)
-                            .then((res) => {
-                              setLoading(false);
-                              router.navigate("/user/edit");
-                            }),
-                        ),
-                        "deleteRecommendation",
-                      );
-                    },
-                    submitText: "Törlés",
-                  }),
-                );
-              },
-            },
-          ]),
-        );
-      loadContacts();
-      return () => {};
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [uid, id, dispatch]),
-  );
+    useFocusEffect(
+      useCallback(() => {
+        loadContacts();
+        setError(null);
+        return () => {};
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+      }, [uid, dispatch]),
+    );
 
-  const save = async () => {
-    if (uid && contact.data && contact.type) {
-      setLoading(true);
-      await supabase.from("contacts").upsert(
-        {
-          ...contact,
-          data: contact.data,
-          type: contact.type,
-          author: uid,
-        },
-        {
-          onConflict: "id",
-        },
-      );
-      setLoading(false);
-      const text = contact.public
-        ? "Bárki láthatja a részleteit az oldaladon, vagy egy bizniszed oldalán."
-        : "Csak az láthatja majd a részleteit, akinek külön engedélyezed.";
-      const onSubmit = () => {
-        router.navigate({ pathname: "/user" });
-      };
-      dispatch(
-        addDialog({
-          title: "Az elérhetőséged elmentetted",
-          text,
-          onSubmit,
-          dismissable: false,
-        }),
-      );
-    }
-  };
-  return (
-    <ThemedView style={{ flex: 1 }}>
-      {!error && (
-        <>
-          <TutorialCard title="Mi az az elérhetőség?">
-            <Text>
-              Ez egy olyan adat, ami megjelenik a profilodon illetve a
-              bizniszeid oldalán. {"\n"}Ez alapján tudnak majd elérni mások.
-            </Text>
-          </TutorialCard>
-          <Dropdown
-            label="Típus"
-            options={types}
-            value={contact?.type}
-            CustomDropdownInput={({
-              placeholder,
-              selectedLabel,
-              label,
-              rightIcon,
-            }: DropdownInputProps) => (
-              <TextInput
-                placeholder={placeholder}
-                label={label}
-                value={selectedLabel}
-                right={rightIcon}
-              />
-            )}
-            menuContentStyle={{ left: 8 }}
-            CustomDropdownItem={({
-              width,
-              option,
-              value,
-              onSelect,
-              toggleMenu,
-              isLast,
-            }) => {
-              return (
-                <>
-                  <TouchableRipple
-                    onPress={() => {
-                      onSelect?.(option.value);
-                      toggleMenu();
-                    }}
+    const saveContact = (c_ind: number, contact: Partial<IContact>) => {
+      if (uid === undefined) return;
+      const newArray = types
+        .map((type, ind) => {
+          const current = contacts.find((c) => c?.type === type.value);
+          if (c_ind === ind) {
+            return {
+              type: type.value,
+              author: uid,
+              data: contact?.data || "",
+              created_at: current?.created_at || new Date().toISOString(),
+              ...current,
+              ...contact,
+            };
+          } else return current;
+        })
+        .filter((item): item is NonNullable<typeof item> => item !== undefined);
+      setContacts(newArray);
+    };
+    const save = async () => {
+      const invalid = contacts.find((c) => c && !c.data && !!c.title);
+      console.log("invalid", invalid);
+
+      if (invalid) {
+        setError({ type: invalid.type, text: "Töltsd ki ezt a mezőt." });
+        return { error: "Invalid field" };
+      }
+      const noNullContacts = contacts.filter((c) => !!c && c.data.length);
+      const deletableIds = contacts
+        .filter((c) => !!c && ((c.title && !c.data) || !c.data))
+        .map((c) => c?.id);
+
+      if (uid && contacts.length) {
+        const response = await supabase
+          .from("contacts")
+          .upsert(noNullContacts, {
+            onConflict: "id",
+            defaultToNull: false,
+          });
+        console.log("TO DEL", deletableIds);
+
+        const del_response = await supabase
+          .from("contacts")
+          .delete()
+          .in("id", deletableIds);
+        console.log("del", del_response);
+
+        return response;
+      }
+    };
+    useImperativeHandle(ref, () => ({
+      async saveContacts() {
+        return await save();
+      },
+    }));
+    return (
+      <View style={{ flex: 1, gap: 8 }}>
+        {types.map((type, ind) => {
+          const current = {
+            title: "",
+            data: "",
+            ...contacts.find((c) => c?.type === type.value),
+          };
+          if (!loading)
+            return (
+              <View key={ind}>
+                <ThemedText type="label">{type.label} elérhetőséged</ThemedText>
+                <TextInput
+                  label={typeToValueLabel(type?.value)}
+                  value={current?.data}
+                  disabled={loading || !type.label}
+                  left={typeToPrefix(type?.value)}
+                  placeholder={typeToPlaceholder(type.value)}
+                  onChangeText={(t) => saveContact(ind, { data: t })}
+                  error={error?.type === type.value}
+                />
+                <TextInput
+                  value={current?.title || ""}
+                  disabled={loading}
+                  label="Leírás (opcionális)"
+                  onChangeText={(t) => saveContact(ind, { title: t })}
+                />
+                {error?.type === type.value && (
+                  <ThemedText type="error" style={{ marginLeft: 16 }}>
+                    {error?.text}
+                  </ThemedText>
+                )}
+                {type.value === "EMAIL" && (
+                  <Button
+                    onPress={() => saveContact(ind, { data: userData?.email })}
                   >
-                    <Headline
-                      style={{
-                        color:
-                          value === option.value
-                            ? MD3DarkTheme.colors.onPrimary
-                            : MD3DarkTheme.colors.primary,
-                        alignItems: "center",
-                        display: "flex",
-                        padding: 8,
-                      }}
-                    >
-                      <Icon source={typeToIcon(option.value)} size={22} />
-                      <ThemedText style={{ marginLeft: 8 }}>
-                        {option.label}
-                      </ThemedText>
-                    </Headline>
-                  </TouchableRipple>
-                  {!isLast && <Divider />}
-                </>
-              );
-            }}
-            CustomMenuHeader={(props) => <></>}
-            onSelect={(t) => {
-              if (t) {
-                const ty = types.find((ty) => ty.value === t);
-                setContact({ ...contact, type: ty?.value });
-              }
-            }}
-          />
-          <TextInput
-            label={typeToValueLabel(contact?.type)}
-            value={contact?.data}
-            disabled={loading || !contact.type}
-            left={typeToPrefix(contact?.type)}
-            placeholder={typeToPlaceholder(contact?.type)}
-            onChangeText={(t) => setContact({ ...contact, data: t })}
-          />
-          <TextInput
-            value={contact?.title || ""}
-            disabled={loading}
-            label="Leírás (opcionális)"
-            onChangeText={(t) => setContact({ ...contact, title: t })}
-          />
-          <TouchableRipple
-            disabled={loading}
-            onPressOut={(e) =>
-              setContact({ ...contact, public: !contact.public })
-            }
-          >
-            <View style={{ flexDirection: "row", padding: 16 }}>
-              <Text style={{ flex: 1 }}>Publikus legyen?</Text>
-              <Switch
-                style={{ marginHorizontal: 16 }}
-                disabled={loading}
-                value={contact?.public || false}
-              />
-            </View>
-          </TouchableRipple>
-          <View style={{ padding: 16 }}>
-            <Text>
-              Ha nem publikus az elérhetőséged akkor csak te engedélyezheted,
-              hogy ki lássa.
-            </Text>
-          </View>
-          <View style={{ padding: 16, gap: 8 }}>
-            <Text>
-              Így fog megjelenni{" "}
-              {contact.public ? "másoknak" : "akinek engedélyezed"}:
-            </Text>
-            <List.Item
-              title={contact.title || contact.data}
-              onPress={() => {}}
-              left={(props) => (
-                <List.Icon {...props} icon={typeToIcon(contact.type)} />
-              )}
-            />
-          </View>
-          <Button
-            onPress={save}
-            style={{ margin: 16 }}
-            mode="contained"
-            loading={loading}
-            disabled={!contact?.data || !contact?.type}
-          >
-            Mentés
-          </Button>
-        </>
-      )}
-      {error && (
-        <ThemedText style={{ textAlign: "center", padding: 16 }}>
-          {error}
-        </ThemedText>
-      )}
-    </ThemedView>
-  );
-};
+                    Regisztrációnál használt email használata
+                  </Button>
+                )}
+              </View>
+            );
+        })}
+      </View>
+    );
+  },
+);
+ContactEditScreen.displayName = "ContactEditScreen";
 
 export default ContactEditScreen;
