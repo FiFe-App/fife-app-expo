f81ce2a913558d78a718d204f2b5be036556aa65
Akos <kristofakos1229@gmail.com>
2025-02-03 12:14:05 +0100

    refactor: enhance ESLint configuration and update user edit component imports

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,36 @@
 module.exports = {
-  extends: ["expo", "prettier",
-    "next/core-web-vitals",
-    "plugin:@typescript-eslint/recommended"],
-  plugins: ["prettier","@typescript-eslint"],
-  parser: "@typescript-eslint/parser",
-  ignorePatterns: ["dist/"],
-  rules: {
-    "prettier/prettier": "warn",
-  },
-};
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:react/recommended","expo", "prettier",
+      "next/core-web-vitals",
+      "plugin:@typescript-eslint/recommended"],
+    plugins: ["prettier","@typescript-eslint",
+        "react"],
+    parser: "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    ignorePatterns: ["dist/"],
+    rules: {
+      "prettier/prettier": "warn",
+    },
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+  };
